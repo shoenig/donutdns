d41fb47ebc0530c2db9ac158c251dab18c1c4a42
Seth Hoenig <seth.a.hoenig@gmail.com>
2021-10-17 11:10:42 -0500

    write some tests for set

--- sources/set/set.go
+++ sources/set/set.go
@@ -6,32 +6,41 @@ type nothing struct{}
 
 var null = nothing{}
 
+// New creates a new Set.
 func New() *Set {
 	return &Set{
-		data: make(map[string]nothing),
+		items: make(map[string]nothing),
 	}
 }
 
+// Set contains a set of unique strings.
 type Set struct {
-	data map[string]nothing
+	items map[string]nothing
 }
 
-func (s *Set) Add(domain string) {
-	s.data[domain] = null
+// Add item to the Set.
+func (s *Set) Add(item string) {
+	s.items[item] = null
 }
 
+// Len returns the number of items in the Set.
 func (s *Set) Len() int {
-	return len(s.data)
+	return len(s.items)
 }
 
+// Union includes all elements of o into the Set.
 func (s *Set) Union(o *Set) {
-	for k := range o.data {
-		s.data[k] = null
+	for k := range o.items {
+		s.items[k] = null
 	}
 }
 
-func (s *Set) Has(name string) bool {
-	clean := strings.TrimRight(name, ".")
-	_, exists := s.data[clean]
+// Has returns whether the Set contains item.
+//
+// For convenience, strip the suffix '.' from item before checking.
+// Optimized for domains, which may or may not contain a trailing period.
+func (s *Set) Has(item string) bool {
+	clean := strings.TrimRight(item, ".")
+	_, exists := s.items[clean]
 	return exists
 }
---
+++ sources/set/set_test.go
@@ -0,0 +1,45 @@
+package set
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Add(t *testing.T) {
+	s := New()
+	require.Equal(t, 0, s.Len())
+
+	s.Add("foo")
+	require.Equal(t, 1, s.Len())
+
+	s.Add("bar")
+	require.Equal(t, 2, s.Len())
+
+	s.Add("foo")
+	require.Equal(t, 2, s.Len())
+}
+
+func Test_Union(t *testing.T) {
+	s1 := New()
+	s1.Add("a")
+	s1.Add("b")
+	s1.Add("c")
+
+	s2 := New()
+	s2.Add("c")
+	s2.Add("d")
+
+	s1.Union(s2)
+	require.Equal(t, 4, s1.Len())
+	require.Equal(t, 2, s2.Len())
+}
+
+func Test_Has(t *testing.T) {
+	s := New()
+	s.Add("a")
+	s.Add("b")
+	require.True(t, s.Has("a"))
+	require.True(t, s.Has("b"))
+	require.False(t, s.Has("c"))
+}
